a58845bd131a751e700d61107dba431b63582176
BM_INFINITY <126513083+BM-INFINITY@users.noreply.github.com>
2026-03-28 21:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def get_data():
     return jsonify(doc if doc else {})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
